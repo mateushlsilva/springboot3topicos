5a187a83cba4f092f43a77c03792021c9f458e57
mateushlsilva <mateushls01@gmail.com>
2024-03-08 08:21:32 -0300

    Adiciona projeto

---
+++ src/main/java/br/gov/sp/fatec/springboot3topicos/service/UsuarioService.java
@@ -0,0 +1,70 @@
+package br.gov.sp.fatec.springboot3topicos.service;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import br.gov.sp.fatec.springboot3topicos.entity.Autorizacao;
+import br.gov.sp.fatec.springboot3topicos.entity.Usuario;
+import br.gov.sp.fatec.springboot3topicos.repository.AutorizacaoRepository;
+import br.gov.sp.fatec.springboot3topicos.repository.UsuarioRepository;
+
+@Service
+public class UsuarioService {
+
+    @Autowired
+    private UsuarioRepository usuarioRepo;
+
+    @Autowired
+    private AutorizacaoRepository autorizacaoRepo;
+
+    public List<Usuario> buscarTodosUsuarios() {
+        List<Usuario> usuarios = usuarioRepo.findAll();
+        return usuarios;
+    }
+
+    public Usuario cadastraUsuario(Usuario usuario) {
+        if(usuario == null ||
+                usuario.getNome() == null ||
+                usuario.getNome().isBlank() ||
+                usuario.getSenha() == null ||
+                usuario.getSenha().isBlank()) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Dados inválidos!");
+        }
+        if(!usuario.getAutorizacoes().isEmpty()) {
+            Set<Autorizacao> autorizacoes = new HashSet<Autorizacao>();
+            for(Autorizacao autorizacao: usuario.getAutorizacoes()) {
+                autorizacao = buscarAutorizacaoPorId(autorizacao.getId());
+                autorizacoes.add(autorizacao);
+            }
+            usuario.setAutorizacoes(autorizacoes);
+        }
+        return usuarioRepo.save(usuario);
+    }
+
+    public Usuario buscarUsuarioPorId(Long id) {
+        Optional<Usuario> usuarioOp = usuarioRepo.findById(id);
+        if(usuarioOp.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Usuário não encontrado!");
+        }
+        return usuarioOp.get();
+    }
+
+    public Autorizacao buscarAutorizacaoPorId(Long id) {
+        Optional<Autorizacao> autorizacaoOp = autorizacaoRepo.findById(id);
+        if(autorizacaoOp.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Autorização não encontrada!");
+        }
+        return autorizacaoOp.get();
+    }
+
+
+
+    
+}
